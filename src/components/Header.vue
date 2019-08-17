685f5a95b8ece4a28f4b58a5b5e515412633a9d7
Samielleuch <Sami.Elleuch@insat.u-carthage.tn>
2019-08-17 11:28:50 +0100

    sup

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <v-toolbar>
+            <v-toolbar-title>Title</v-toolbar-title>
+            <v-spacer></v-spacer>
+        </v-toolbar>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1> Question</h1>
+</template>
+
+<script>
+    export default {
+        name: "Question Box"
+    }
+</script>
+
+<style scoped>
+
+</style>
